1271ef1867ac0a0fb91b292a284196abf71f4934
Ivan Chaplinskyi <i.chaplinskyi@astoundcommerce.com>
2023-03-28 15:21:23 +0300

    Refactoring code from Vitaliy.

--- app/utils/todo.server.ts
+++ app/utils/todo.server.ts
@@ -1,10 +1,5 @@
 import { prisma } from "./prisma.server";
 
-export async function createTodo(todo: string, getUserId: string) {
-  await prisma.todo.create({
-    data: {
-      taskName: todo,
-      userId: getUserId,
-    },
-  });
+export async function createTodo(taskName: string, userId: string) {
+  await prisma.todo.create({ data: { taskName,  userId }});
 }
